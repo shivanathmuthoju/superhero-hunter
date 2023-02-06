2b54a1c82cf0d5dbe4e7890ec28b475aa08e06f3
shivanathmuthoju <124571452+shivanathmuthoju@users.noreply.github.com>
2023-02-07 00:44:48 +0530

    local data string

--- favourites.js
+++ favourites.js
@@ -26,7 +26,7 @@ let favouriteContainer = document.querySelector(".favourites__container");
 window.addEventListener('load',() => {
 
     let localData = JSON.parse(localStorage.getItem(storedFavourites));
-    console.log(localData, "LocalData);
+    console.log(localData, "LocalData");
     console.log(typeof(localData));
     favouritesList = localData;
     console.log("Favourites loaded")
